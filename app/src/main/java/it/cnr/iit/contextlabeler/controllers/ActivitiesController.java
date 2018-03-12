a0333d1b27b9b0c5da89bf5e105d2cfdb1911a16
mattia <mattia@mcampana.iit.cnr.it>
2018-03-12 15:57:10 +0100

    .

--- app/src/main/java/it/cnr/iit/contextlabeler/controllers/ActivitiesController.java
+++ app/src/main/java/it/cnr/iit/contextlabeler/controllers/ActivitiesController.java
@@ -13,18 +13,18 @@ public class ActivitiesController {
     public static List<ActivityElement> getActivities(){
 
         List<ActivityElement> activities = new ArrayList<>();
-        activities.add(new ActivityElement(R.drawable.trip, "Trip"));
-        activities.add(new ActivityElement(R.drawable.relax, "Relax"));
+        //activities.add(new ActivityElement(R.drawable.trip, "Trip"));
+        //activities.add(new ActivityElement(R.drawable.relax, "Relax"));
         activities.add(new ActivityElement(R.drawable.cinema, "Cinema"));
         activities.add(new ActivityElement(R.drawable.theater, "Theater"));
         activities.add(new ActivityElement(R.drawable.nightlife, "Nightlife"));
-        activities.add(new ActivityElement(R.drawable.fast_food, "Fast Food"));
-        activities.add(new ActivityElement(R.drawable.eating, "Eating"));
-        activities.add(new ActivityElement(R.drawable.wine_bar_pub, "Wine bar/Pub"));
-        activities.add(new ActivityElement(R.drawable.driving, "Driving"));
-        activities.add(new ActivityElement(R.drawable.study, "Studying"));
-        activities.add(new ActivityElement(R.drawable.reading, "Reading"));
-        activities.add(new ActivityElement(R.drawable.tv, "Watching TV"));
+        //activities.add(new ActivityElement(R.drawable.fast_food, "Fast Food"));
+        activities.add(new ActivityElement(R.drawable.eating, "Launch Break"));
+        //activities.add(new ActivityElement(R.drawable.wine_bar_pub, "Wine bar/Pub"));
+        //activities.add(new ActivityElement(R.drawable.driving, "Driving"));
+        //activities.add(new ActivityElement(R.drawable.study, "Studying"));
+        //activities.add(new ActivityElement(R.drawable.reading, "Reading"));
+        //activities.add(new ActivityElement(R.drawable.tv, "Watching TV"));
         activities.add(new ActivityElement(R.drawable.shopping, "Shopping"));
         activities.add(new ActivityElement(R.drawable.physical_activity, "Physical exercise"));
         activities.add(new ActivityElement(R.drawable.free_time, "Free time"));
